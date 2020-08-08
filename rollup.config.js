2126a98ffb26508e029a7359ffaf0df4dc3445bd
Riza Dwi Arifiyanto <riza.arifiyanto@indosatooredo.com>
2020-08-09 02:40:00 +0700

    - install gh-pages - publish application to github

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
-
+import ghPages from 'gh-pages';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -50,7 +50,9 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser() && ghPages.publish('public', (err) => {
+			console.log('Published to github', err);
+		})
 	],
 	watch: {
 		clearScreen: false
